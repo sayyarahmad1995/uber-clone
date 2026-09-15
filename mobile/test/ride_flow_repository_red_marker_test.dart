db3ab9311a29788e1626d31ab521a740e1b718d3
Sayyar Ahmad <m.sayyarahmad1995@gmail.com>
2026-09-16 00:00:23 +0500

    test: require semantic repository API

---
+++ mobile/test/ride_flow_repository_red_marker_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:uber_clone/features/ride_flow/ride_flow_repository.dart';
+
+void main() {
+  test('repository API is semantic', () {
+    expect(RideFlowRepository, isNotNull);
+  });
+}
