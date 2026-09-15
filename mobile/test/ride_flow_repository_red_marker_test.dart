ac8df6be6271422f9dfbc4a2731434cc0cf32257
Sayyar Ahmad <m.sayyarahmad1995@gmail.com>
2026-09-16 00:00:37 +0500

    test: remove temporary repository marker

--- mobile/test/ride_flow_repository_red_marker_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:uber_clone/features/ride_flow/ride_flow_repository.dart';
-
-void main() {
-  test('repository API is semantic', () {
-    expect(RideFlowRepository, isNotNull);
-  });
-}
